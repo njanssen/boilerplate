6e4fa90ad77f76046ae3b8add6cf34d318040e59
Niels Janssen <niels@vliegwerk.com>
2023-04-08 00:09:29 +0200

    Added lerping and opacities

--- p5/p5-orca/public/sketch.js
+++ p5/p5-orca/public/sketch.js
@@ -1,5 +1,7 @@
 let size;
-let heights = Array.from(new Array(3),() => 0.5);
+
+let heights = Array.from(new Array(3), () => 0.5);
+const ys = {};
 
 function setup() {
     socket = io.connect('http://localhost:3000');
@@ -8,30 +10,34 @@ function setup() {
         console.log('Connected to socket server');
     });
 
-    socket.on('data', (path,...data) => {
-        console.log('Orca data',path, data);
+    socket.on('data', (path, ...data) => {
+        console.log('Orca data', path, data);
         heights = data;
     });
 
     size = min(windowWidth, windowHeight);
     createCanvas(size, size);
 }
+
 function draw() {
-    background(255, 125, 0);
+    background(220, 220, 220,10);
 
     noFill();
-    strokeWeight((10 * size) / 200);
+    strokeWeight((10 * size) / 150);
 
-    let y1 = heights.length >= 1 ? heights[0] : 0.5;
-    let y2 = heights.length >= 2 ? heights[1] : 0.5;
-    let y3 = heights.length >= 3 ? heights[2] : 0.5;
+    ys.y1_prev = ys.y1 ? ys.y1 : 0.5;
+    ys.y2_prev = ys.y2 ? ys.y2 : 0.5;
+    ys.y3_prev = ys.y3 ? ys.y3 : 0.5;
+    ys.y1 = lerp(ys.y1_prev, heights.length >= 1 ? heights[0] : 0.5, 0.3);
+    ys.y2 = lerp(ys.y2_prev, heights.length >= 2 ? heights[1] : 0.5, 0.3);
+    ys.y3 = lerp(ys.y3_prev, heights.length >= 3 ? heights[2] : 0.5, 0.3);
 
-    stroke(255, 0, 125);
-    circle(0.25 * size, y1 * size, 0.15 * size);
+    stroke(255, 0, 125, 90);
+    circle(0.5 * size, ys.y1 * size, 0.45 * size);
 
-    stroke(125, 0, 255);
-    circle(0.5 * size, y2 * size, 0.15 * size);
+    stroke(125, 0, 255, 90);
+    circle(0.5 * size, ys.y2 * size, 0.5 * size);
 
-    stroke(0, 125, 255);
-    circle(0.75 * size, y3 * size, 0.15 * size);
+    stroke(0, 125, 255, 90);
+    circle(0.5 * size, ys.y3 * size, 0.55 * size);
 }
